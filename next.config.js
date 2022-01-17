5dd20ee1a910c9b1db3ff0adf90dc1d1f79ce7d7
egoheru <egoherokugit@gmail.com>
2022-01-17 07:42:25 +0100

    updated

---
+++ next.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    images: {
+      domains: ['images.unsplash.com'],
+    },
+  }
+
+
+// module.exports = {
+//     images: {
+//         domains: ['images.unsplash.com']
+//     }
+// }
